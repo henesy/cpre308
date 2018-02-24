d5d58020645fd54dcf09900d3c5cfd826e9bedd6
Sean Hinchee <henesy.dev@gmail.com>
2018-02-24 14:26:26 -0600

    final version before cleanup

--- lab4/src/src/test3.sh
+++ lab4/src/src/test3.sh
@@ -9,27 +9,27 @@ echo "FILE acidpaper.ps"
 echo "DESCRIPTION From a language"
 echo "PRINTER black_white"
 echo "PRINT"
-sleep 5
+sleep 2
 echo "NEW"
 echo "NAME output2.pdf"
 echo "FILE samplec.ps"
 echo "DESCRIPTION The second test file"
 echo "PRINTER color"
 echo "PRINT"
-sleep 5
+sleep 2
 echo "NEW"
 echo "NAME output3.pdf"
 echo "FILE mkfiles.ps"
 echo "DESCRIPTION Mkfiles"
 echo "PRINTER black_white"
 echo "PRINT"
-sleep 5
+sleep 2
 echo "NEW"
 echo "NAME output4.pdf"
 echo "FILE pathstar.ps"
 echo "DESCRIPTION Inferno OS"
 echo "PRINTER color"
 echo "PRINT"
-sleep 5
+sleep 2
 echo "EXIT"
 
